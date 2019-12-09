a76326ffdc9222b12b91e7aaa85660fe7b8c59c1
li091611 <374010745@qq.com>
2019-12-09 10:53:20 +0800

    lijingwei

--- src/components/shooping/index.vue
+++ src/components/shooping/index.vue
@@ -1,22 +1,30 @@
 <template>
-    <div>
-    购物车
+  <div>
+    <div class="top">
+        <i></i>
+        <span>购物车</span>
+        <i></i>
     </div>
+    <div class="log">
+        登录后可同步账户购物车中的商品
+        <button>登录</button>
+    </div>
+    <div class="shopping">
+        选购的东西
+    </div>
+  </div>
 </template>
 <script>
 // @ is an alias to /src
 export default {
-    name: 'XXX',
-    data() {
-        return {
-        
-        }
-    },
-    components: {
-        
-    }
-}
+  name: "XXX",
+  data() {
+    return {
+      dataList: []
+    };
+  },
+  components: {}
+};
 </script>
 <style lang="less">
-
 </style>
