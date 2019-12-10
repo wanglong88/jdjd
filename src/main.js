4097103c362774998704f2a62e6378c526eb18fa
wanglong88 <337283331@qq.com>
2019-12-10 10:41:03 +0800

    he

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/less/common.less'
-<<<<<<< HEAD
-import { Tabbar, TabbarItem ,Icon,Search,Swipe, SwipeItem,Lazyload,Grid, GridItem,CountDown    } from "vant";
-Vue.use(Tabbar).use(TabbarItem).use(Icon ).use(Search).use(Swipe).use(SwipeItem).use(Lazyload).use(Grid).use(GridItem).use(CountDown);
-=======
-import { Tabbar, TabbarItem, Button, Icon, Card, Tag, SubmitBar, Checkbox, CheckboxGroup, Stepper, Sku ,Popup, Cell } from "vant";
-Vue.use(Tabbar).use(TabbarItem).use(Button).use(Icon).use(Card).use(SubmitBar).use(Checkbox).use(CheckboxGroup).use(Popup).use(Stepper).use(Tag).use(Sku).use(Cell);
->>>>>>> origin/dev-v1.0
 import 'vant/lib/index.css';
+// import { Tabbar, TabbarItem, Icon,  } from "vant";
+
+// Vue.use(Tabbar).use(TabbarItem).use(Icon);
+
+import { Tabbar, TabbarItem, Button, Icon, Card, Tag, SubmitBar, Checkbox, CheckboxGroup, Stepper, Sku, Popup,Search, Swipe, SwipeItem, Lazyload, Grid, GridItem, CountDown,Cell } from "vant";
+
+Vue.use(Tabbar).use(TabbarItem).use(Button).use(Icon).use(Card).use(SubmitBar).use(Tag).use(Checkbox).use(CheckboxGroup).use(Stepper).use(Sku).use(Popup).use(Search).use(Swipe).use(SwipeItem).use(Lazyload).use(Grid).use(GridItem).use(CountDown).use(Cell);
+
+// 
 Vue.config.productionTip = false
 
 new Vue({
