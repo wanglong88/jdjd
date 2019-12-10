8667535d8adc050b5221271cf1a1d798ce2ae339
daishengliang123 <1596747222@qq.com>
2019-12-10 11:01:33 +0800

    dsl

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Tabbar, TabbarItem } from "vant";
-Vue.use(Tabbar).use(TabbarItem);
+
+
+
+import { Tabbar, TabbarItem ,Icon ,Search, Tab, Tabs, TreeSelect, Grid, GridItem} from "vant";
+Vue.use(Tabbar).use(TabbarItem).use(Icon).use(Search).use(Tab).use(Tabs);
+Vue.use(Grid).use(GridItem);
+Vue.use(TreeSelect);
 import 'vant/lib/index.css';
+
 Vue.config.productionTip = false
 
 new Vue({
