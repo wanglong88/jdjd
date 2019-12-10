be3b55b9f2537bf3af498ea0d471ad9aaf32d5ff
wanglong88 <337283331@qq.com>
2019-12-10 15:40:31 +0800

    1210-2

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,20 +2,23 @@
   <div>
     <router-view></router-view>
     <van-tabbar v-model="active">
-      <van-tabbar-item  to="/home">
-      <img src="../assets/logo/download.png" class="logos" alt="">
+      <van-tabbar-item to="/home">
+        <img src="../assets/logo/download.png" class="logos" alt />
       </van-tabbar-item>
-      <van-tabbar-item  to="/classify">
-      <img src="../assets/logo/2.png" class="logos" alt="">
+      <van-tabbar-item to="/classify">
+        <img src="../assets/logo/2.png" class="logos" alt />
       </van-tabbar-item>
-      <van-tabbar-item >
-        <img src="../assets/logo/3.gif" class="logos" alt="">
+      <van-tabbar-item>
+        
+        <a href="https://wq.jd.com/webportal/event/27358?ptag=138097.1.19&sceneval=2">
+          <img src="../assets/logo/3.gif" class="logos" alt />
+        </a>
       </van-tabbar-item>
-      <van-tabbar-item  to="/shooping">
-      <img src="../assets/logo/4.png" class="logos" alt="">
+      <van-tabbar-item to="/shooping">
+        <img src="../assets/logo/4.png" class="logos" alt />
       </van-tabbar-item>
-      <van-tabbar-item  to="/user">
-      <img src="../assets/logo/5.png" class="logos" alt="">
+      <van-tabbar-item to="/user">
+        <img src="../assets/logo/5.png" class="logos" alt />
       </van-tabbar-item>
     </van-tabbar>
   </div>
@@ -34,7 +37,7 @@ export default {
 };
 </script>
 <style lang="less" scoped>
-.logos{
+.logos {
   width: 16vw;
 }
 </style>
